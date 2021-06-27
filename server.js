0af0caf1ec4c5b5fac523ce8ed91f953865e0339
Anamaria Costea <costea.anamaria1412@gmail.com>
2021-06-27 19:10:56 +0300

    enabling auth

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var zetta = require('zetta');
-var { enableAuth } = require('./auth')
 var PORT = process.env.PORT || 3000;
 
 zetta()
   .name('cloud')
-  .use(enableAuth)
+  .use(function (server) {
+    const cloud = server.httpServer.cloud
+
+    cloud.use(function (handle) {
+      handle('request', function (env, next) {
+        if (env.request.method === 'POST') {
+          env.argo._routed = true
+          env.response.statusCode = 200
+          env.response.statusMessage = "OK"
+        }
+        next(env)
+      })
+    })
+  })
   .expose('*')
-  .listen(PORT, function(err) {
-    if(err) {
+  .listen(PORT, function (err) {
+    if (err) {
       console.error(err);
       process.exit(1);
     }
