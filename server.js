492dccde657b5df0529bfb4897c7e818671493ef
Anamaria Costea <costea.anamaria1412@gmail.com>
2021-06-27 19:18:28 +0300

    enabling auth

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ var PORT = process.env.PORT || 3000;
 
 zetta()
   .name('cloud')
-  .use(function (server) {
-    const cloud = server.httpServer.cloud
+  // .use(function (server) {
+  //   const cloud = server.httpServer.cloud
 
-    cloud.use(function (handle) {
-      handle('request', function (env, next) {
-        // if (env.request.method === 'POST') {
-          env.argo._routed = true
-        //   env.response.statusCode = 200
-        //   env.response.statusMessage = "OK"
-        // }
-        next(env)
-      })
-    })
-  })
+  //   cloud.use(function (handle) {
+  //     handle('request', function (env, next) {
+  //       // if (env.request.method === 'POST') {
+  //         env.argo._routed = true
+  //       //   env.response.statusCode = 200
+  //       //   env.response.statusMessage = "OK"
+  //       // }
+  //       next(env)
+  //     })
+  //   })
+  // })
   .expose('*')
   .listen(PORT, function (err) {
     if (err) {
