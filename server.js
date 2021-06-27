be44c2ea0280e605cd6ffd28de44236dc674d65e
Anamaria Costea <costea.anamaria1412@gmail.com>
2021-06-27 19:16:19 +0300

    enabling auth

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ zetta()
 
     cloud.use(function (handle) {
       handle('request', function (env, next) {
-        if (env.request.method === 'POST') {
+        // if (env.request.method === 'POST') {
           env.argo._routed = true
-          env.response.statusCode = 200
-          env.response.statusMessage = "OK"
-        }
+        //   env.response.statusCode = 200
+        //   env.response.statusMessage = "OK"
+        // }
         next(env)
       })
     })
